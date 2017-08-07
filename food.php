d9a17a699ceb5ed64e208f89533c183f6ff634c0
britoluisea <britoluiea@gmail.com>
2017-08-07 16:21:29 -0430

    separacion de la estructura del documento

---
+++ food.php
@@ -0,0 +1,11 @@
+<?php include"footer.php"; ?>
+    <!-- /.container -->
+    <!-- jQuery -->
+    <script src="userfiles/js/jquery.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="userfiles/js/bootstrap.min.js"></script>
+    <!-- Script   -->
+    <script> 
+    </script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php include"head.php"; ?>
+<body>
+<?php include"nav.php"; ?>
+<?php include"header.php"; ?>
+
+    <!-- Page Content -->
+    <div class="container">
+        <!-- Marketing Icons Section -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">
+                    Welcome to Modern Business
+                </h1>
+            </div>
+        </div> 
+    </div>
+
+
+<?php include"food.php"; ?> 
