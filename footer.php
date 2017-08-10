5e5fe355e8197752cd13aceb73c72a36d0131eb7
britoluisea <britoluiea@gmail.com>
2017-08-10 12:50:17 -0430

    20% mas alto el slider y se cambio el texto de copy del footer

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
         <footer>
             <div class="container">
                 <div class="col-lg-12">
-                    <p>Copyright &copy; All Rights Reserved | by:<a href="http://amwebpro.com/"> amwebpro.com</a></p>
+                    <p>Design & Development by: <a href="http://amwebpro.com/"> AM Web Pro Inc</a></p>
                 </div>
             </div>
         </footer>
