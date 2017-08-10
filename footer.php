ad8752321fffc853484df8a527071163386302c5
britoluisea <britoluiea@gmail.com>
2017-08-10 12:39:54 -0430

    retoques en el responsive para el logo menu y welcome

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
         <!-- Footer -->
-        <footer> 
+        <footer>
             <div class="container">
                 <div class="col-lg-12">
                     <p>Copyright &copy; All Rights Reserved | by:<a href="http://amwebpro.com/"> amwebpro.com</a></p>
                 </div>
             </div>
-        </footer> 
+        </footer>
